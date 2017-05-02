a40b02c1ba60e0a50587b4d78ca96b64f86c62d1
Fabi <Fabi@TERMINATOR>
2017-05-02 16:34:25 +0200

    Initial commit

---
+++ app/src/main/java/edu/uv/students/mobiledevices/sensorbasedpositioning/Positioning.java
@@ -0,0 +1,13 @@
+package edu.uv.students.mobiledevices.sensorbasedpositioning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Positioning extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_positioning);
+    }
+}
