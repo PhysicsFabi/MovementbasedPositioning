6c4f7931d15ccd08a37754f127958827e08e4df6
Fabi <Fabi@TERMINATOR>
2017-05-23 18:48:17 +0200

    completes Visualization

---
+++ app/src/main/java/edu/uv/students/mobiledevices/sensorbasedpositioning/positionreconstruction/interfaces/OnSensorAccuracyLowListener.java
@@ -0,0 +1,8 @@
+package edu.uv.students.mobiledevices.sensorbasedpositioning.positionreconstruction.interfaces;
+
+/**
+ * Created by Fabi on 23.05.2017.
+ */
+
+public interface OnSensorAccuracyLowListener {
+}
--- app/src/main/java/edu/uv/students/mobiledevices/sensorbasedpositioning/visualization/OnResetListener.java
+++
@@ -1,9 +0,0 @@
-package edu.uv.students.mobiledevices.sensorbasedpositioning.visualization;
-
-/**
- * Created by Fabi on 13.05.2017.
- */
-
-public interface OnResetListener {
-    void onReset();
-}
